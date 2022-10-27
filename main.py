039e8ef9baf1a752356ab615e76fa47a3c2ecd64
henry <1151071+HenrYxZ@users.noreply.github.com>
2022-10-26 22:04:18 -0300

    feat: add without interest comparison

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, \
-    QLineEdit, QLabel, QVBoxLayout, QWidget, QHBoxLayout
+    QLineEdit, QLabel, QWidget, QVBoxLayout, QFormLayout
 
 
 class MainWindow(QMainWindow):
@@ -10,38 +10,36 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Fire Calculator")
 
         layout = QVBoxLayout()
+        form_box = QFormLayout()
 
-        monthly_row = QHBoxLayout()
         label_monthly = QLabel("Monthly contrib: ")
         self.input_monthly = QLineEdit("2000")
         self.input_monthly.returnPressed.connect(self.calculate)
-        monthly_row.addWidget(label_monthly)
-        monthly_row.addWidget(self.input_monthly)
+        form_box.addRow(label_monthly, self.input_monthly)
 
-        rate_row = QHBoxLayout()
         label_rate = QLabel("Rate: ")
         self.input_rate = QLineEdit("5")
         self.input_rate.returnPressed.connect(self.calculate)
-        rate_row.addWidget(label_rate)
-        rate_row.addWidget(self.input_rate)
+        form_box.addRow(label_rate, self.input_rate)
 
-        years_row = QHBoxLayout()
         label_years = QLabel("Years: ")
         self.input_years = QLineEdit("25")
         self.input_years.returnPressed.connect(self.calculate)
-        years_row.addWidget(label_years)
-        years_row.addWidget(self.input_years)
+        form_box.addRow(label_years, self.input_years)
 
         self.button = QPushButton("Calculate")
         self.button.clicked.connect(self.calculate)
+        form_box.addRow(self.button)
 
-        self.label = QLabel(f"${0}")
+        self.total_label = QLabel(f"${0}")
+        self.total_label.setStyleSheet("font-weight: bold")
+        self.total_no_interest_label = QLabel(f"${0}")
+        self.extra_label = QLabel(f"${0}")
 
-        layout.addLayout(monthly_row)
-        layout.addLayout(rate_row)
-        layout.addLayout(years_row)
-        layout.addWidget(self.button)
-        layout.addWidget(self.label)
+        layout.addLayout(form_box)
+        layout.addWidget(self.total_label)
+        layout.addWidget(self.total_no_interest_label)
+        layout.addWidget(self.extra_label)
 
         container = QWidget()
         container.setLayout(layout)
@@ -55,9 +53,19 @@ class MainWindow(QMainWindow):
 
         # formula from https://www.wallstreetiswaiting.com/running-the-numbers-1/calculating-interest-recurring-payments/
         final = annual * ((1 + annual_rate) ** years - 1) / annual_rate
+        self.total_label.setText("Total: ${:,.0f}".format(final))
 
-        final_str = "{:,.0f}".format(final)
-        self.label.setText(f"${final_str}")
+        # Calculate total with no interest to compare
+        total_no_interest = annual * years
+        self.total_no_interest_label.setText(
+            "Total without interest: ${:,.0f}".format(total_no_interest)
+        )
+
+        # Compound interest
+        extra = final - total_no_interest
+        self.extra_label.setText(
+            "Obtained from interest: ${:,.0f}".format(extra)
+        )
 
 
 if __name__ == '__main__':
